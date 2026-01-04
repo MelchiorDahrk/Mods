71c7fd907da20829a5b8b20c3ef9ebbdf32c8fd5
MelchiorDahrk <47513579+MelchiorDahrk@users.noreply.github.com>
2026-01-04 17:14:05 -0500

    Updated for White Wave 1.5

--- The Search for the White Wave/01 OpenMW Addons/scripts/SSQN/Iconlists/TheSearchForTheWhiteWave.lua
+++ The Search for the White Wave/01 OpenMW Addons/scripts/SSQN/Iconlists/TheSearchForTheWhiteWave.lua
@@ -1,5 +1,5 @@
 local icons = {}
 -- The Search for the White Wave
-		icons.mdWW_Journal = "\\Icons\\mdww\\q\\quest_whitewave.tga"
+		icons.mdWW_Journal = "\\Icons\\mdww\\q\\quest_whitewave.dds"
 
 return icons
